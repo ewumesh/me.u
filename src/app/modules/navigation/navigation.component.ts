92bf93a98d291a09ac84c1754289a303cb154d57
Umesh Thapa <umeshthapa112233@gmail.com>
2022-08-05 10:04:22 +0545

    initial commit

---
+++ src/app/modules/navigation/navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-name',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.html']
+})
+export class NavigationComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
