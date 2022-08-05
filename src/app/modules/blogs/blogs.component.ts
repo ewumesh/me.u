64c3a5859cc798be54211a401839d357c751d834
Umesh Thapa <39909267+umeshthapa12@users.noreply.github.com>
2022-08-05 08:19:28 +0000

    update

---
+++ src/app/modules/blogs/blogs.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    templateUrl: './blogs.component.html',
+    styleUrls: ['blogs.css']
+})
+
+export class BlogsComponent {
+    
+}
---
+++ src/app/modules/blogs/blogs.module.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+
+import { BlogsComponent } from "./blogs.component";
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [BlogsComponent],
+    exports: []
+})
+
+export class BlogsModule {
+
+}
